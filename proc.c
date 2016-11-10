0f3179a21e7bd0d136987baeaeec5e4b0057d2a2
Suwamik <spaul@stuy.edu>
2016-11-09 21:14:15 -0500

    Add files via upload

--- proc.c
+++ proc.c
@@ -5,21 +5,20 @@
 
 static void sighandler (int signo){
   if(signo == SIGINT){
-    printf("seen");
+    printf("exited due to SIGINT\n");
     exit(0);
+  }
+  if (signo == SIGUSR1) {
+    printf("ppid: %d\n",getppid());
   }
 }
 
-
-
-
 int main(){
+  signal(SIGINT, sighandler);
+  signal(SIGUSR1,sighandler);
   while(1){
-    signal(SIGINT, sighandler);
-    printf("%d\n", getpid());
+    printf("pid: %d\n", getpid());
     sleep(1);
   }
-  
-
   return 0;
 }
